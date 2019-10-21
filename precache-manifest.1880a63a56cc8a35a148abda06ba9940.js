6f230a3a778a67c0767bb6b461f6c1b5f554f68c
Connie Ye <constanceye1316@gmail.com>
2019-10-21 18:00:40 -0400

    nice

---
+++ precache-manifest.1880a63a56cc8a35a148abda06ba9940.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580bd0a585fda769667c11d64f77ed1f",
+    "url": "/sephora/index.html"
+  },
+  {
+    "revision": "7d9157f565cef5148ac5",
+    "url": "/sephora/static/css/main.e0614a38.chunk.css"
+  },
+  {
+    "revision": "90a45182400e30983404",
+    "url": "/sephora/static/js/2.52bfb6bf.chunk.js"
+  },
+  {
+    "revision": "7d9157f565cef5148ac5",
+    "url": "/sephora/static/js/main.f2da60b4.chunk.js"
+  },
+  {
+    "revision": "4821326320fdfcfb1067",
+    "url": "/sephora/static/js/runtime-main.f577bd63.js"
+  },
+  {
+    "revision": "d036747814b227e57059b656104e11af",
+    "url": "/sephora/static/media/blackStar.d0367478.svg"
+  },
+  {
+    "revision": "6247637576bf7d9282ab8e5b3aea7b0a",
+    "url": "/sephora/static/media/logo.62476375.svg"
+  },
+  {
+    "revision": "f02a9cc48ed93dd5af0de8b96bbae622",
+    "url": "/sephora/static/media/whiteStar.f02a9cc4.svg"
+  }
+]);
